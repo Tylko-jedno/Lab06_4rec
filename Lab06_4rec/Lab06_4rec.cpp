048ad764174f76cfd7e3611fbeb0a15f12e8d74c
Tylko-jedno <diogkek@gmail.com>
2024-10-15 21:38:35 +0300

    first master commit

---
+++ Lab06_4rec/Lab06_4rec.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+void Sort(int* a, const int size, int i)
+{
+    int max = a[i];
+    int imax = i;
+    for (int j = i + 1; j < size; j++)
+        if (max < a[j])
+        {
+            max = a[j];
+            imax = j;
+        }
+
+    if (a[i] % 2 > 0) {
+        a[imax] = a[i];
+        a[i] = max;
+    }
+
+
+    if (i < size - 2)
+        Sort(a, size, i + 1);
+}
+
+int Max(int* a, const int size, int i, int max)
+{
+    if (a[i] > max)
+        max = a[i];
+    if (i < size - 1)
+        return Max(a, size, i + 1, max);
+    else
+        return max;
+}
+
+int Min(int* a, const int size, int i, int min)
+{
+    if (a[i] < min)
+        min = a[i];
+    if (i < size - 1)
+        return Min(a, size, i + 1, min);
+    else
+        return min;
+}
+
+void f1(int* a, const int size, int& s, int& d, int low, int high, int i) {
+    if (i < size) {
+        if (a[i] > 0)
+            s += a[i];
+        if (a[i] >= low && a[i] <= high)
+            d *= a[i];
+        f1(a, size, s, d, low, high, i + 1);
+    }
+}
+
+void Create(int* a, const int size, const int Low, const int High, int i)
+{
+    a[i] = Low + rand() % (High - Low + 1);
+    if (i < size - 1)
+        Create(a, size, Low, High, i + 1);
+}
+
+void Print(int* a, const int size, int i)
+{
+    cout << setw(4) << a[i];
+    if (i < size - 1)
+        Print(a, size, i + 1);
+    else
+        cout << endl;
+}
+
+int main()
+{
+    int n;
+    cout << "n = "; cin >> n;
+
+    int* a = new int[n];
+    Create(a, n, -10, 10, 0);
+    Print(a, n, 0);
+
+    int s = 0;
+    int d = 1;
+
+    int low = Min(a, n, 0, a[0]);
+    int high = Max(a, n, 0, a[0]);
+
+    f1(a, n, s, d, low, high, 0);
+    cout << "s = " << s << endl;
+    cout << "d = " << d << endl;
+
+    Sort(a, n, 0);
+    Print(a, n, 0);
+
+    delete[] a;
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,76 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab06_4rec/Lab06_4rec.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int a[22];
+
+			Create(a, 22, -10, 35, 0);
+
+			int expected[22] = { 31,  11,  22, -6,  23,  28,  14,   0, -4,  28, -9,  29, -5,  27,  15,  21, -5,  18,  33, -2, -3,  12 };
+
+			for (int i = 0; i < 22; i++)
+			{
+				Assert::AreEqual(expected[i], a[i], L"Elements are not equal", LINE_INFO());
+			}
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			int a[22] = { 10, -2,   3,   9,   7,   6,   2, -10 };
+			int s_expected = 37;
+			int d_expected = 453600;
+
+			int s = 0;
+			int d = 1;
+			f1(a, 8, s, d, -10, 10, 0);
+
+
+			Assert::AreEqual(s_expected, s, 0.0000001);
+			Assert::AreEqual(d_expected, d, 0.0000001);
+
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			int a[8] = { 10, -2 ,  3,   9,   7,   6,   2, -10 };
+
+			Sort(a, 8, 0);
+
+			int expected[22] = { 10, -2,   9,   7,   6,   3,   2, -10 };
+
+			for (int i = 0; i < 8; i++)
+			{
+				Assert::AreEqual(expected[i], a[i], L"Elements are not equal", LINE_INFO());
+			}
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			int a[22] = { 10, -2,   3,   9,   7,   6,   2, -10 };
+			
+			int expected = -10;
+			int res = Min(a, 8, 0, 10);
+
+
+			Assert::AreEqual(expected, res, 0.0000001);
+		}
+		TEST_METHOD(TestMethod5)
+		{
+			int a[22] = { 10, -2,   3,   9,   7,   6,   2, -10 };
+
+			int expected = 10;
+			int res = Max(a, 8, 0, -10);
+
+
+			Assert::AreEqual(expected, res, 0.0000001);
+		}
+	};
+}
